d27c5f4df48acef158919bc46e17c0d8fda1bad5
root <root@playground.mzdev.local>
2019-05-31 04:01:25 +0000

    test2

--- test.sh
+++ test.sh
@@ -1,37 +1,78 @@
 #!/bin/bash
+nowtime=$(TZ="Asia/Seoul" date '+%Y%m%d_%Hh%Mm%Ss')
 
 # region check #
 region=$1
 tmpfile=$(mktemp /tmp/region.XXXXXXXXX)
 aws ec2 describe-regions --output text | cut -f3 > $tmpfile
-if [[ $region == "" ]] ; then echo -e "region input : $0 {\e[38;3;1mREGION NAME\e[0m} "; cat $tmpfile && rm -rf $tmpfile;exit 1;elif [ $(cat $tmpfile | grep $region | wc -l) -eq 0 ];then echo -e " \e[38;5;1mREGION NAME checking plz.\e[0m : $region "; cat $tmpfile && rm -rf $tmpfile ;exit 1;fi
+#if [[ $region == "" ]] ; then echo -e "region input : $0 {\e[38;3;1mREGION NAME\e[0m} "; cat $tmpfile && rm -rf $tmpfile;exit 1;elif [ $(cat $tmpfile | grep $region | wc -l) -eq 0 ];then echo -e " \e[38;5;1mREGION NAME checking plz.\e[0m : $region "; cat $tmpfile && rm -rf $tmpfile ;exit 1;fi
+if [[ $region == "" ]] ; then 
+	echo -e "region input : $0 {\e[38;3;1mREGION NAME\e[0m} "
+	cat $tmpfile && rm -rf $tmpfile
+	exit 1
+elif [ $(cat $tmpfile | grep $region | wc -l) -eq 0 ];then 
+	echo -e " \e[38;5;1mREGION NAME checking plz.\e[0m : $region "
+	cat $tmpfile && rm -rf $tmpfile 
+	exit 1
+fi
 
 # ec2 list file name #
 running_ec2_list=list_${region}.txt
 
-######## SSH KEY ########
-#mzdev-public="./key/pub"
-#mzdev-private="./key/pri"
-#mzdev-dmz="./key/dmz"
-#
-######## Amazon AMI  #########
-
-#ami-078e96948945fc2c9="ubuntu"
-#ami-067c32f3d5b9ace91="ubuntu"
-#ami-06cf2a72dadf92410="centos"
+# default dir create#
+backup_dir="/data_backup/"
+backup_dir_region="${backup_dir}${region}/"
+backup_dir_log="${backup_dir}log/${region}/"
+mkdir -p ${backup_dir_region}
+mkdir -p ${backup_dir_log}
 
 # ec2 list IP save #
 aws ec2 --region $1 describe-instances --filters "Name=instance-state-name,Values=running"  --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`]|[0].Value,PrivateIpAddress,ImageId,KeyName]' --output=text > $running_ec2_list
 ##echo >> Hostname, Private IP, AMI ID, key pair ####
-#sampel >> local.mzdev.www 10.99.101.81    ami-078e96948945fc2c9   mzdev-private ####
-#for i in {16..21} {21..16} ; do echo -en "\e[48;5;${i}m \e[0m" ; done ; echo
-#cat $running_ec2_list
 
 
+ssh_connection(){
+	if [ $(curl -ksfL $ec2_pri_ip:22 --connect-timeout 3 | grep SSH | wc -l) -eq 0 ]; then 
+		echo " >> $ec2_hostname($ec2_pri_ip) security group checking $nowtime << " >> ${backup_dir_log}${ec2_hostname}
+		ec2_hostname=""
+	fi
+}
 
-for i in {196..201} {201..196} ; do echo -en "\e[48;5;${i}m M Z C\e[0m" ; done ; echo
-printf "%-25s %-15s %-21s %-20s\n" "Hostname" "Private IP" "AMI ID" "Key pair name"
-echo "------------------------- --------------- --------------------- ---------------"
+
+test_print(){
+	if [[ $ec2_hostname != "" ]]; then 
+		printf "%-23s %-15s %-21s %-18s" $ec2_hostname $ec2_pri_ip $ec2_ami_id $ec2_key_name #$ec2_user $key_pair
+		echo ""
+	fi
+}
+test_line(){
+	for i in {196..201} {201..196} ; do echo -en "\e[48;5;${i}m M Z C\e[0m" ; done ; echo
+#	for i in {196..200} {200..196} ; do echo -en "\e[48;5;${i}m Megazone \e[0m" ; done ; echo
+}
+
+ssh_remote_run(){
+	if [[ $ec2_hostname != "" ]]; then 
+		mkdir -p ${backup_dir_region}${ec2_hostname}
+		remote_command="uname -v"
+		#ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ssh_command_log.txt
+		ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ${backup_dir_log}${ec2_hostname} 
+	#	scp -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip:/data/AAAAAAAAAAAAAAA ${backup_dir_region}${ec2_hostname}/${ec2_hostname}_${nowtime}.tar.gz
+	fi
+}
+
+ssh_remote_backup(){
+	if [[ $ec2_hostname != "" ]]; then 
+		mkdir -p ${backup_dir_region}${ec2_hostname}
+		remote_command="uname -v"
+		#ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ssh_command_log.txt
+		ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ${backup_dir_log}${ec2_hostname} 
+	#	scp -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip:/data/AAAAAAAAAAAAAAA ${backup_dir_region}${ec2_hostname}/${ec2_hostname}_${nowtime}.tar.gz
+	fi
+}
+
+
+test_line
+echo "----------------------- --------------- --------------------- -------------"
 while read ec2_hostname ec2_pri_ip ec2_ami_id ec2_key_name; do
 	case $ec2_ami_id in
 		ami-078e96948945fc2c9)
@@ -39,7 +80,7 @@ while read ec2_hostname ec2_pri_ip ec2_ami_id ec2_key_name; do
 		ami-067c32f3d5b9ace91)
 			ec2_user="ubuntu";;
 		ami-06cf2a72dadf92410)
-			ec2_user="cetnos";;
+			ec2_user="centos";;
 	esac
 	case $ec2_key_name in
 		mzdev-public)
@@ -49,12 +90,18 @@ while read ec2_hostname ec2_pri_ip ec2_ami_id ec2_key_name; do
 		mzdev-dmz)
 			key_pair="dmz";;
 	esac
-	printf "%-25s %-15s %-21s %-20s" $ec2_hostname $ec2_pri_ip $ec2_ami_id $ec2_key_name $ec2_user $key_pair
-	echo ""
-	ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip 'ls -al /data' < /dev/null
-done < $running_ec2_list
-for i in {196..201} {201..196} ; do echo -en "\e[48;5;${i}m M Z C\e[0m" ; done ; echo
+	case $ec2_hostname in
+		kr.mzdev.relay)
+			ec2_hostname="";;
+	esac
 
+test_print #test
+ssh_connection # ssh_connection TEST
+ssh_remote_run #test remote command chk
+done < $running_ec2_list
+test_line
+echo " >> Directory : $backup_dir_region "
+echo " >> Logs : $backup_dir_log"
 
 ############ clear file
 rm -rf $tmpfile
