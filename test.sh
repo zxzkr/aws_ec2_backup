d09e24dd65a4affee44f37b0b0bdb789287024eb
root <root@playground.mzdev.local>
2019-05-31 05:51:08 +0000

    z

--- test.sh
+++ test.sh
@@ -19,6 +19,9 @@ fi
 # ec2 list file name #
 running_ec2_list=list_${region}.txt
 
+# backup run #
+backuprun=$2
+
 # default dir create#
 backup_dir="/data_backup/"
 backup_dir_region="${backup_dir}${region}/"
@@ -50,58 +53,70 @@ test_line(){
 #	for i in {196..200} {200..196} ; do echo -en "\e[48;5;${i}m Megazone \e[0m" ; done ; echo
 }
 
+
 ssh_remote_run(){
 	if [[ $ec2_hostname != "" ]]; then 
 		mkdir -p ${backup_dir_region}${ec2_hostname}
-		remote_command="uname -v"
-		#ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ssh_command_log.txt
-		ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ${backup_dir_log}${ec2_hostname} 
-	#	scp -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip:/data/AAAAAAAAAAAAAAA ${backup_dir_region}${ec2_hostname}/${ec2_hostname}_${nowtime}.tar.gz
+	#	ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ${backup_dir_log}${ec2_hostname} 
+	#	echo $(TZ="Asia/Seoul" date) ":::copy::::::::" $(scp -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip:/data/${ec2_hostname}.tar.gz ${backup_dir_region}${ec2_hostname}/${ec2_hostname}_${nowtime}.tar.gz < /dev/null) >> ${backup_dir_log}${ec2_hostname}
+#		echo $(TZ="Asia/Seoul" date) ":::delete::::::" $(ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "sudo find /data -name ${ec2_hostname}.tar.gz -mtime +1 -delete" < /dev/null) >> ${backup_dir_log}${ec2_hostname} 
+	echo "zzzzzzzzz?????????????????????? $ec2_hostname ssh remote run ???"
 	fi
 }
 
 ssh_remote_backup(){
 	if [[ $ec2_hostname != "" ]]; then 
 		mkdir -p ${backup_dir_region}${ec2_hostname}
-		remote_command="uname -v"
-		#ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ssh_command_log.txt
-		ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "$remote_command" < /dev/null >> ${backup_dir_log}${ec2_hostname} 
-	#	scp -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip:/data/AAAAAAAAAAAAAAA ${backup_dir_region}${ec2_hostname}/${ec2_hostname}_${nowtime}.tar.gz
+#		echo $(TZ="Asia/Seoul" date) ":::delete::::::" $(ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "sudo find /data -name ${ec2_hostname}.tar.gz -mtime +1 -delete" < /dev/null) >> ${backup_dir_log}${ec2_hostname} 
+#		echo $(TZ="Asia/Seoul" date) ":::compression:"$(ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "sudo tar zcfp /data/${ec2_hostname}.tar.gz /data/" < /dev/null) >> ${backup_dir_log}${ec2_hostname} 
+#		echo $(TZ="Asia/Seoul" date) ":::disk space::"$(ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "sudo df -h /data | tail -1" < /dev/null) >> ${backup_dir_log}${ec2_hostname}
+		echo $(TZ="Asia/Seoul" date) ":::disk space::"$(ssh -oStrictHostKeyChecking=no -i /data_backup/key/$key_pair $ec2_user@$ec2_pri_ip "sudo df -h /data | tail -1" < /dev/null) >> ${backup_dir_log}${ec2_hostname} && cat ${backup_dir_log}${ec2_hostname} |tail -1 #echo test
 	fi
 }
 
 
-test_line
-echo "----------------------- --------------- --------------------- -------------"
-while read ec2_hostname ec2_pri_ip ec2_ami_id ec2_key_name; do
-	case $ec2_ami_id in
-		ami-078e96948945fc2c9)
-			ec2_user="ubuntu";;
-		ami-067c32f3d5b9ace91)
-			ec2_user="ubuntu";;
-		ami-06cf2a72dadf92410)
-			ec2_user="centos";;
-	esac
-	case $ec2_key_name in
-		mzdev-public)
-			key_pair="pub";;
-		mzdev-private)
-			key_pair="pri";;
-		mzdev-dmz)
-			key_pair="dmz";;
-	esac
-	case $ec2_hostname in
-		kr.mzdev.relay)
-			ec2_hostname="";;
-	esac
+#test_line
+list_check(){
+#	echo "----------------------- --------------- --------------------- -------------"
+	while read ec2_hostname ec2_pri_ip ec2_ami_id ec2_key_name; do
+		case $ec2_ami_id in
+			ami-078e96948945fc2c9)
+				ec2_user="ubuntu";;
+			ami-067c32f3d5b9ace91)
+				ec2_user="ubuntu";;
+			ami-06cf2a72dadf92410)
+				ec2_user="centos";;
+		esac
+		case $ec2_key_name in
+			mzdev-public)
+				key_pair="pub";;
+			mzdev-private)
+				key_pair="pri";;
+			mzdev-dmz)
+				key_pair="dmz";;
+		esac
+		case $ec2_hostname in
+			kr.mzdev.relay)
+				ec2_hostname="";;
+		esac
 
-test_print #test
-ssh_connection # ssh_connection TEST
-ssh_remote_run #test remote command chk
-done < $running_ec2_list
+	ssh_connection # ssh_connection TEST
+		#data backup?
+		if [[ $backuprun == "start" ]]; then
+			ssh_remote_backup
+		else
+			test_print #test
+			#ssh_remote_run #test remote command chk
+		fi
+	done < $running_ec2_list
+}
+list_check
 test_line
-echo " >> Directory : $backup_dir_region "
-echo " >> Logs : $backup_dir_log"
+
+#############################################################################################
+# info
+#echo " >> Directory : $backup_dir_region "
+#echo " >> Logs : $backup_dir_log"
 
 ############ clear file
 rm -rf $tmpfile
